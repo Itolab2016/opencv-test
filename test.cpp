9c3848d9d7536b437c11867cac7e0e9c8bf56d38
16pure <mmommo1206@gmail.com>
2016-06-18 10:11:50 +0900

    test.cpp
    lenaを出す

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+  // 画像データをファイル（この例では「lena.jpg」）から読み込む
+  cv::Mat src = cv::imread("lena.jpg", cv::IMREAD_COLOR);
+
+  // 画像の読み込みに失敗したらエラー終了する
+  if(src.empty())
+  {
+    std::cerr << "Failed to open image file." << std::endl;
+    return -1; 
+  }
+
+  cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
+  cv::imshow("image", src);
+  cv::waitKey(0);
+  cv::destroyAllWindows();
+
+  return 0;
+}
