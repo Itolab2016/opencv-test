5d33e5b54ec32919627d9ac5e15d584e5430fa97
kouhei.ito@itolab-ktc.com <kouhei.ito@itolab-ktc.com>
2016-06-18 16:07:09 +0900

    matの謎解明

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
@@ -6,6 +7,17 @@ int main(int argc, const char* argv[])
 {
   // 画像データをファイル（この例では「lena.jpg」）から読み込む
   cv::Mat src = cv::imread("lena.jpg", cv::IMREAD_COLOR);
+	printf("%d,%d,%d\n",src.cols,src.rows,src.step);
+	printf("%p\n",src.data);
+	printf("%p,%p,%d\n",src.datastart,src.dataend,src.dataend-src.datastart);
+	printf("%d\n",src.data[0]);
+	short i,j;
+	for (i=0;i<src.rows;i++){
+		for (j=0;j<src.cols;j++){
+			src.data[i*src.step+j*3+1]=0;
+		}
+	}
+
 
   // 画像の読み込みに失敗したらエラー終了する
   if(src.empty())
