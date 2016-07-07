1bf58081cca51638a3fbd93c482c229f0c12b4b3
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-07-07 20:52:51 +0900

    キャリブレーション用calib.cpp追加
    カメラキャリブレーションについて研究するため

---
+++ calib.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+//カメラキャリブレーションの試験プログラム
+//現在作成中・・・
+
+int main(){
+
+}
+
