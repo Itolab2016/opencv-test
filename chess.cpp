9c59251f35c613292ec1efa0c4dc1f80394dc88f
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-07-07 17:00:05 +0900

    chess.cpp変更
    高精度化とコメントの追加

--- chess.cpp
+++ chess.cpp
@@ -13,15 +13,16 @@ using namespace std;
 
 int main(){
   Mat img;
-  //Mat gray;
+  Mat gray;
 
   img = imread("chessboard2.jpg");
-  //cvtColor(img, gray, COLOR_BGR2GRAY);
+  cvtColor(img, gray, COLOR_BGR2GRAY);
 
   Size boardSize(5, 9);
   vector<Point2f> pointBuf;
 
  
+  //コーナ検出
   bool found = findChessboardCorners( img,
                                  boardSize,
                                  pointBuf,
@@ -32,10 +33,19 @@ int main(){
   //このフラグを有効にした場合は、コーナー検出が失敗する。
   //多くのWebサイトでサンプルにはこのフラグが有効になっているが
   //私の実験ではすべて失敗したので、コメントアウトされている
-
+  
   cout << "Find? " << found << endl;
 
+  //サブピクセル精度に向上
+  cornerSubPix( gray, pointBuf, boardSize, Size(-1, -1),
+                TermCriteria(CV_TERMCRIT_ITER |
+                CV_TERMCRIT_EPS, 20, 0.01));
+
+
+
   drawChessboardCorners(img, boardSize, Mat(pointBuf), found);
+
+  //検出結果描画
   namedWindow("chessboard", CV_WINDOW_NORMAL);
   imshow("chessboard",img);
 
