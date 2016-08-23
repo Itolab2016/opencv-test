e05d827a5abb9626cc062efb125b91f9da32fc25
kouhei.ito@itolab-ktc.com <kouhei.ito@itolab-ktc.com>
2016-08-23 15:49:14 +0900

    modfy opticalflow.cpp

--- opticalflow.cpp
+++ opticalflow.cpp
@@ -9,9 +9,13 @@ using namespace cv;
 int main(int argc, const char* argv[])
 {
 	char str[10];
-	Mat img1,img2;
+	const int width=1280;
+	const int height=720;
+	Mat img1(height, width, CV_8UC1);
+	Mat img2(height, width, CV_8UC1);
 
-	namedWindow("image", cv::WINDOW_AUTOSIZE);
+	namedWindow("image1", cv::WINDOW_NORMAL);
+	namedWindow("image2", cv::WINDOW_NORMAL);
 
 	for (int i=0; i<100; i++){
 		  
@@ -25,14 +29,16 @@ int main(int argc, const char* argv[])
 			return -1; 
 		}
 		//x方向微分
-		for (int y=0; y<300; y++){
-			for (int x=0; x<300; x++){
-				img2.at<unsigned char>(y, x) =img1.at<unsigned char>(y,x+1)-img1.at<unsigned char>(y, x); 
+		for (int y=0; y<height; y++){
+			for (int x=0; x<width-5; x++){
+				img2.at<unsigned char>(y, x) =img1.at<unsigned char>(y, x+5) - img1.at<unsigned char>(y, x);
+				//printf("%d\n",img2.at<unsigned char>(y,x) ); 
 			}	
 		}
 		//img2=img1.clone();
-		imshow("image", img2);
-		if(waitKey(60)=='q')break;
+		imshow("image1", img1);
+		imshow("image2", img2);
+		if(waitKey(300)=='q')break;
 	}
 
 	destroyAllWindows();
