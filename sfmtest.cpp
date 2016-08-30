3b4a99a6054b70c2efeb0fbef684c949b7d7cd03
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-08-30 18:59:43 +0900

    research sfm

--- sfmtest.cpp
+++ sfmtest.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
 
 using namespace std;
 using namespace cv;
@@ -31,9 +34,10 @@ public:
 
 my3dpoint::my3dpoint(){
   double harf=RAND_MAX/2.0;
-  x=((rand()-harf)/harf)*10.0;
-  y=((rand()-harf)/harf)*10.0;
-  z=((rand()-harf)/harf)*10.0;
+  x=((rand()-harf)/harf)*100.0;
+  y=((rand()-harf)/harf)*200.0;
+  z=300.0+((double)rand()/RAND_MAX)*500.0;
+
 
 }
 
@@ -44,24 +48,90 @@ my3dpoint::my3dpoint(){
 int main()
 {
 
-  my3dpoint p1[100];
   // Example. Estimation of fundamental matrix using the RANSAC algorithm
-  int point_count = 100;
-  vector<Point2f> points1(point_count);
-  vector<Point2f> points2(point_count);
+  int point_count = 1000;
+
+  char file2dv1[20]="test2dv1.dat";
+  char file2dv2[20]="test2dv2.dat";
+  char file2dv3[20]="test2dv3.dat";
+  char file3d[20]  ="test3d.dat";
+
+  my3dpoint p1[point_count];
+  vector<Point2f> points1;
+  vector<Point2f> points2;
+  vector<Point2f> points3;
   vector<Point3f> point3d1(point_count);
   vector<Point3f> point3d2(point_count);
-  Mat K = (Mat_<float>(3,3) << 1, 0, 320, 0, 1, 240, 0, 0, 1);
-  float tx=1,ty=0,tz=0;
-  Mat R1 = (Mat_<float>(3,4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 );
-  Mat R2 = (Mat_<float>(3,4) << 1, 0, 0, tx, 0, 1, 0, ty, 0, 0, 1, tz, 0, 0, 0, 1 );
+  float fx=300.0;
+  float fy=300.0;
+  float cx=0.0;
+  float cy=0.0;
+  Mat K = (Mat_<float>(3,3) << fx, 0, cx, 0, fy, cy, 0, 0, 1);
+  //float tx=1,ty=0,tz=0;
+  //Mat R1 = (Mat_<float>(3,4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 );
+  //Mat R2 = (Mat_<float>(3,4) << 1, 0, 0, tx, 0, 1, 0, ty, 0, 0, 1, tz, 0, 0, 0, 1 );
 
-//  cout << K << endl;
+  for(int i=0; i<point_count; i++){
+    point3d1[i]=Point3f(p1[i].getX(),p1[i].getY(),p1[i].getZ());
+  }
+  //cout << point3d1 << endl;
+  
+  
+  float rx=0.0,ry=0.0,rz=3.14159*20/180;
+  Mat rvec1=(Mat_<float>(3,1)<<0,0,0);
+  Mat rvec2=(Mat_<float>(3,1)<<rx,ry,rz);
+
+  float tx=0,ty=0,tz=0;
+  float scale=sqrt(tx*tx+ty*ty+tz*tz);
+  Mat tvec1=(Mat_<float>(3,1)<<0,0,0);
+  Mat tvec2=(Mat_<float>(3,1)<<tx,ty,tz);
+  
+  Mat distcoef;
+    
+  //透視投影実験
+  projectPoints(point3d1,rvec1,tvec1,K,distcoef,points1);
+  projectPoints(point3d1,rvec2,tvec2,K,distcoef,points2);
+
+  //エッセンシャル行列,Rとt を求める
+  Mat E=findEssentialMat(points1,points2,300.0,Point2d(0,0),RANSAC,0.999,1.0);
+  Mat R,t;
+
+  recoverPose(E,points1,points2,R,t,300.0,Point2d(0,0));
+
+  //再投影
+  projectPoints(point3d1,R,scale*t,K,distcoef,points3);
+
+  //ファイルに記録
+  ofstream fp2dv1(file2dv1);
+  ofstream fp2dv2(file2dv2);
+  ofstream fp2dv3(file2dv3);
+  ofstream fp3d(file3d);
+
+  for (int i=0; i<point_count;i++){
+    fp3d<<point3d1[i].x<<" "<<point3d1[i].y<<" "<<point3d1[i].z<<endl;
+    fp2dv1<<points1[i].x<<" "<<points1[i].y<<endl;
+    fp2dv2<<points2[i].x<<" "<<points2[i].y<<endl;
+    fp2dv3<<points3[i].x<<" "<<points3[i].y<<endl;
+  }  
+
+
+
+  Mat R2;
+  Rodrigues(rvec2,R2);  
+  cout << "R=" << R <<endl<<endl;
+  cout << "R2=" << R2 <<endl<<endl;
+
+  //cout << "rvec=" << rvec << endl <<endl;
+  //printf("Rvec=[%8.3f,%8.3f,%8.3f]\n",rvec.at<double>(0,0),rvec.at<double>(1,0),rvec.at<double>(2,0));
+  //cout << "t=" << t << endl;
+  printf("tvec=[%4.0f,%4.0f,%4.0f]\n",scale*t.at<double>(0,0),scale*t.at<double>(1,0),scale*t.at<double>(2,0));
+  
+
 //  cout << R1 << endl;
 //  cout << K*R2 << endl;
-  for (int i=0;i<100;i++){
-    p1[i].dispnobr();
-  }
+//  for (int i=0;i<100;i++){
+//    p1[i].dispnobr();
+//  }
 
 #if 0
   // initialize the points here ... */
